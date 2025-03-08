bced0f6e26a7ae9ea381afab80ea85898b4902f7
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-03-08 21:52:45 +0530

    header-bugFix

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { headerData } from "../utility/utilData";
+import clsx from "clsx";
 
 const Header = () => {
-  const [isOpen, setisOpen] = useState<boolean>(false);
+  const [isOpen, setisOpen] = useState(false);
   const [hasScrolled, setHasScrolled] = useState<boolean>(false);
 
   const Link = ({ title, id }: { title: string; id: string }) => {
@@ -67,9 +68,9 @@ const Header = () => {
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
         <div
-          className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-[#0C1838] max-lg:opacity-${
-            isOpen ? 100 : 0
-          }`}
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none")}
         >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto ">
