295e64ef33fb8b90fd2d8a60598048baf92e2c89
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-03-16 14:08:58 +0530

    added testimonials

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Features from "./sections/Features"
 import Header from "./sections/Header"
 import Hero from "./sections/Hero"
 import Pricing from "./sections/Pricing"
+import Testimonials from "./sections/Testimonials"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Features/>
       <Pricing/>
       <Faq/>
+      <Testimonials/>
     </main>
   )
 }
